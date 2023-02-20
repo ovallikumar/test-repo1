1edbc43a47c13509d69fca7a695db3ae37526d87
dev1 <dev1@mail.com>
2023-02-20 20:12:04 +0530

    mdf dev1

--- hello.java
+++ hello.java
@@ -1,25 +1,11 @@
-<<<<<<< HEAD
-class #from dev1
-=======
-<<<<<<< HEAD
-class
+class from dev1
 {
-	public static void main("hello");
+	public void static main(hello vallikumar)
 	{
-		systemprintln("hello from dev2")
-=======
-class 
->>>>>>> refs/remotes/origin/main
-{
-	public static void main ("hello"); #from dev1
-	{
-<<<<<<< HEAD
-	 	systemprintln("hello form dev1") #from dev1
-=======
-	 	systemprintln("hello form dev1")
->>>>>>> 7673b877c54c23298193a2f5953953545991efdf
->>>>>>> refs/remotes/origin/main
+		println("hello vallikumar");
 	}
+
 }
+
 
 
